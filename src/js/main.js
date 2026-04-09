6e805268e7408df6d05975b269a092c58f741b4e
Riina <riina.e.aaltonen@gmail.com>
2026-04-09 09:47:11 +0200

    Fått till en fungerande modal med information och design. Påbörjar tillägg av nytt API.

--- src/js/main.js
+++ src/js/main.js
@@ -45,19 +45,64 @@ async function fetchMovies(endpoint) {
 
 function displayMovies(movies, sectionID) {
 
-    let section = document.querySelector(sectionID); 
+  let section = document.querySelector(sectionID);
 
-    movies.slice(0, 8).forEach(movie => {
-      const div = document.createElement("div"); 
-      div.classList.add("movie-card"); 
+  movies.slice(0, 8).forEach(movie => {
+    const div = document.createElement("div");
+    div.classList.add("movie-card");
 
-      div.innerHTML = `
+    div.innerHTML = `
       <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+      <div class="overlay">
+        <span class="addIcon material-symbols-outlined">bookmark_add</span>
+        <span class="infoIcon material-symbols-outlined">info</span>
+      </div>
       `;
 
-      section.appendChild(div); 
+    section.appendChild(div);
 
-    })
+    let infoIcon = div.querySelector(".infoIcon");
+    infoIcon.addEventListener("click", () => {
+      displayModal(movie);
+    });
 
-    
+  })
+}
+
+function displayModal(movie) {
+  
+  const modal = document.querySelector(".modal");
+  const modalContent = document.querySelector(".modal-content");
+
+  
+
+  modalContent.innerHTML = `
+    <div class="poster">
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+    </div>
+
+    <div class="about">
+      <h2>${movie.title}</h2>
+      <div class="sub-menu">
+        <div class="submenu-items">
+          <span class="star-icon material-symbols-outlined">star_rate</span>
+          <p>${movie.vote_average}<p>
+        </div>
+        <div class="submenu-items">
+          <span class="add-icon material-symbols-outlined">bookmark_add</span>
+          <span>Lägg till på Watchlist</span>
+        </div>
+      </div>
+      <p>${movie.overview}</p>
+    </div>
+  `;
+
+  modal.appendChild(modalContent); 
+
+  modal.style.display = "block"; 
+
+  const closeIcon = document.querySelector(".closeIcon"); 
+  closeIcon.addEventListener("click", () => {
+    modal.style.display = "none"; 
+  })
 }
